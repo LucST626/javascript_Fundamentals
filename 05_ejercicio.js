4cb90fb7eae1ffeebf6c9889790747b25e02537f
Luc626 <lucasst626@gmail.com>
2023-10-22 15:17:18 +0200

    Ejercicio 5

---
+++ 05_ejercicio.js
@@ -0,0 +1,9 @@
+// Definir la función areaCirculo
+function areaCirculo(radio) {
+    var area = Math.PI * radio * radio;
+    console.log("El área del círculo con radio " + radio + " es: " + area);
+    return area;
+  }
+  
+  // Llamar función con valor  radio específico
+  areaCirculo(5); 
