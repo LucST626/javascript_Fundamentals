16559d8b5f7b0baf4c50d8f91a3399770999d170
Luc626 <lucasst626@gmail.com>
2023-10-22 14:59:18 +0200

    Ejercicio 3

---
+++ 03_ejercicio.js
@@ -0,0 +1,8 @@
+var usuario = {
+    nombre: "Lucas",
+    edad: 30, // Cambia 30 por tu edad real
+    ciudad: "Granada"
+  };
+  
+  var saludo = `Hola, me llamo ${usuario.nombre}, tengo ${usuario.edad} años y soy de ${usuario.ciudad}.`;
+  console.log(saludo);
